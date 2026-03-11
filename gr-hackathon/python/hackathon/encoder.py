a1304c95bd8c779715eeb97186c87c0f827b8db9
shira1805ozeri <shiraozeri3@gmail.com>
2026-03-11 09:00:35 +0200

    just pushed it so it would save the version before i start changing it

--- gr-hackathon/python/hackathon/encoder.py
+++ gr-hackathon/python/hackathon/encoder.py
@@ -7,35 +7,60 @@
 #
 
 
-import numpy
+import numpy 
 from gnuradio import gr
 
 class encoder(gr.sync_block):
     """
     docstring for block encoder
     """
-    def __init__(self, p0,p1, p2,p3,p4):
+    def __init__(self):
         gr.sync_block.__init__(self,
             name="encoder",
             in_sig=None,
             out_sig=[numpy.float32, ])
         
-        self.__string_input__ = p0
-        self.__string_input__ = [1,0,1,1,0]
-        self.__pn__ = p1
-        self.__pn__ = [1,-1,1,1,-1,1]
+        self.__sps__ = 2
+        self.__string_input__ = numpy.random.randint(0, 2, 20)
+        self.__pn__ = numpy.random.randint(0, 2, 100)
         
-    def mosulate_info(string, pn):
-        bpsk_string = string.replace(0, -1)
-        modulated = []
-        for elemant in bpsk_string:
-            modulated = modulated + [elemant * x for x in pn]
+    def modulate_info(self,string, pn, n):
         
-        return modulated
+        # add preamble
+        string = numpy.concatenate(([1, 1, 1, 1, 1], string))
+        #xor info with pn sequence
+        new_pn = numpy.tile(pn, len(string))
+        new_string = numpy.repeat(string, len(pn))
+        data_to_mod = new_string ^ new_pn
         
+        # pulse shape
+        data_to_mod = numpy.repeat(data_to_mod, self.__sps__)
+        print("len of info is {}".format(len(data_to_mod)))
+
+        # make sure we have exactly n samples to output
+        if len(data_to_mod) < n:
+            data_to_mod = numpy.concatenate([data_to_mod, numpy.random.randint(0, 2, n - len(data_to_mod))])
+        if len(data_to_mod) > n:
+            data_to_mod = data_to_mod[0:n]
+            # to change
+
+        # bpsk modulation: 0 -> -1, 1 -> +1:
+        data_to_mod[data_to_mod == 0] = -1
+
+        return data_to_mod.astype(numpy.float32)
+        
+
 
     def work(self, input_items, output_items):
         out = output_items[0]
-        # <+signal processing here+>
-        out[:] = mosulate_info(self.__string_input__, self.__pn__ )
-        return len(output_items[0])
+
+        # number of samples requested
+        n = len(out)
+
+        # generate random 0/1 array
+        bits = self.modulate_info(self.__string_input__, self.__pn__, n)
+
+        # copy to output buffer
+        out[:] = bits
+
+        return len(bits)
